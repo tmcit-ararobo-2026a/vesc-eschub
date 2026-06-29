241f34ee541b944524e563a6bb87b69ac7195457
kanai0914 <ayuayugonbe@gmail.com>
2026-06-30 03:10:34 +0900

    feat:hole_senserを読み取るもの

--- encoder/src/encoder.cpp
+++ encoder/src/encoder.cpp
@@ -22,6 +22,16 @@ constexpr uint32_t k_heartbeat_toggle_interval_ms = 500;
 
 uint32_t heartbeat_last_toggle_time_ms = 0;
 
+uint8_t motor_num;
+
+int32_t motor_stop_count      = 10;
+float vesc_velo[4]            = {0.0f, 0.0f, 0.0f, 0.0f};
+float rpm_conversion_constant = -45000.0f;
+float target_rpm              = 0.0f;
+int32_t rotate_count          = 0;
+bool is_moving                = false;
+bool magnet_near              = false;
+bool homing                   = false;
 /**
  * @brief Toggle heartbeat LED at a fixed interval.
  */
@@ -46,6 +56,8 @@ void do_homing()
     vesc.comm_can_set_rpm(45, 0);
     vesc.comm_can_set_rpm(43, 0);
 
+    homing       = false;
+    rotate_count = 0;
     HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
 }
 
@@ -53,16 +65,60 @@ void setup()
 {
     fdcan1_driver.init();
     vesc.init();
-    HAL_ADC_Start(&hadc1);
+    HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
 
-    uint8_t motor_num;
-    while (!esc_hub.get_init(motor_num, motor_config_belt));
+    while (!esc_hub.get_init(motor_num, motor_config_belt)) {
+        HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_SET);
+    }
+
+    HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_RESET);
 
     do_homing();
 }
 
 void loop()
 {
+    if (esc_hub.get_angular_velocities(vesc_velo)) {
+        is_moving =
+            (vesc_velo[0] != 0.0f || vesc_velo[1] != 0.0f || vesc_velo[2] != 0.0f ||
+             vesc_velo[3] != 0.0f);
+    }
+
+    if (is_moving) {
+        HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
+    } else {
+        HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
+    }
+
+    HAL_ADC_Start(&hadc1);                       // 変換開始
+    HAL_ADC_PollForConversion(&hadc1, 100);      // 完了待ち
+    int32_t adc_val = HAL_ADC_GetValue(&hadc1);  // 値取得
+    float voltage   = (float)adc_val / 4095.0f * 3.3f;
+
+    target_rpm = vesc_velo[0] * rpm_conversion_constant;
+
+    if (rotate_count < motor_stop_count) {
+        vesc.comm_can_set_rpm(45, target_rpm);
+        vesc.comm_can_set_rpm(43, target_rpm);
+    } else {
+        vesc.comm_can_set_rpm(45, 0);
+        vesc.comm_can_set_rpm(43, 0);
+    }
+
+    if (voltage > 2.0f && !magnet_near) {
+        rotate_count++;
+        magnet_near = true;
+    } else if (magnet_near && voltage < 1.7f) {
+        magnet_near = false;
+    }
+
+    if (rotate_count == motor_stop_count) {
+        homing = true;
+    }
+
+    if (homing) {
+        do_homing();
+    }
     update_heartbeat_led();
     HAL_Delay(10);
 }
